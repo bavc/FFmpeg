5eb64e883a88b1e9615889bb65f86f5af0e0d029
Mark Heath (@utopia) <mjpeg0@silicontrip.net>
2013-05-31 07:35:25 +1000

    Changed chroma size calculation.  Used pointers for previous frame.

--- libavfilter/vf_values.c
+++ libavfilter/vf_values.c
@@ -42,7 +42,7 @@
  16 April 07:10 - 7:40
  17 April 16:20 - 17:00
  18 April 7:10 - 8:10, 12:50 - 13:05
- 29 Apr 7:15-8:00, 
+ 29 Apr 7:15-8:00,
  */
 
 /* Prototypes for filter functions */
@@ -71,23 +71,23 @@ static const char *const filter_names[] = { "tout", "vrep",  NULL };
 typedef struct
 {
     const AVClass *class;
-
+    
     FILE  *fh;
     char *filename;
-
+    
     int chromah;
     int chromaw;
     int fc;
-
+    
     int fs;
     int cfs;
-
+    
     enum FilterMode outfilter;
     int filter[FILT_NUMB];
     char *statistics_str;
-
+    
     AVFrame *frame_prev;
-
+    
 } valuesContext;
 
 #define OFFSET(x) offsetof(valuesContext, x)
@@ -112,20 +112,20 @@ static av_cold int init(AVFilterContext *ctx, const char *args)
 {
     valuesContext *values = ctx->priv;
     int ret;
-
+    
     av_log(ctx, AV_LOG_DEBUG, ">>> init().\n");
-
+    
     values->class = &values_class;
-
+    
     av_opt_set_defaults(values);
-
+    
     av_log(ctx, AV_LOG_DEBUG, "    init() av_set_options_string.\n");
-
+    
     if ((ret = av_set_options_string(values, args, "=", ":")) < 0)
         return ret;
-
+    
     // parse statistics filter string
-
+    
     do {
         char *next,*cur=values->statistics_str;
         int fil,ok;
@@ -133,14 +133,14 @@ static av_cold int init(AVFilterContext *ctx, const char *args)
             next = strchr(cur,'|');
             if (next)
                 *next++=0;
-
+            
             ok=0;
             for (fil = 0; fil < FILT_NUMB; fil ++) {
-
+                
                 if (strcmp(filter_names[fil],cur)==0)
                 {
                     av_log(ctx,AV_LOG_DEBUG, "Found filter: %s\n",filter_names[fil]);
-
+                    
                     ok = 1;
                     values->filter[fil] = 1;
                 }
@@ -152,19 +152,19 @@ static av_cold int init(AVFilterContext *ctx, const char *args)
                 return AVERROR(EINVAL);
             }
             cur = next;
-
+            
         }
     } while(0);
     //
-
+    
     values->fc = 0;
     values->fh = NULL;
-
+    
     if (values->filename != NULL)
         values->fh = fopen (values->filename,"w");
-
+    
     av_log(ctx, AV_LOG_DEBUG, "<<< init().\n");
-
+    
     return 0;
 }
 
@@ -173,9 +173,9 @@ static av_cold void uninit(AVFilterContext *ctx)
     valuesContext *values = ctx->priv;
     if (values->fh)
         fclose(values->fh);
-
-    av_frame_free(&values->frame_prev);
-
+    
+    //    av_frame_free(&values->frame_prev);
+    
 }
 
 static int query_formats(AVFilterContext *ctx)
@@ -185,76 +185,76 @@ static int query_formats(AVFilterContext *ctx)
         PIX_FMT_YUV444P,  PIX_FMT_YUV422P,  PIX_FMT_YUV420P, PIX_FMT_YUV411P,
         PIX_FMT_NONE
     };
-
+    
     ff_set_common_formats(ctx, ff_make_format_list(pix_fmts));
     return 0;
 }
 
 static int config_props(AVFilterLink *outlink)
 {
-
+    
     AVFilterContext *ctx = outlink->src;
     valuesContext *values = ctx->priv;
     AVFilterLink *inlink = outlink->src->inputs[0];
-
+    
     int hsub, vsub;
-
+    
     av_log(ctx, AV_LOG_DEBUG, ">>> config_props().\n");
-
+    
     avcodec_get_chroma_sub_sample(outlink->format, &hsub, &vsub);
-
+    
     outlink->w = inlink->w;
     outlink->h = inlink->h;
-
+    
     values->chromaw = -((-inlink->w) >> hsub);
     values->chromah = -((-inlink->h) >> vsub);
-
+    
     values->fs = inlink->w * inlink->h;
     values->cfs = values->chromaw * values->chromah;
-
+    
     values->frame_prev = av_frame_alloc();
     values->frame_prev->width  = inlink->w;
     values->frame_prev->height = inlink->h;
     values->frame_prev->format = inlink->format;
-
+    
     if (av_frame_get_buffer(values->frame_prev, 32) < 0)
     {
         av_frame_free(&values->frame_prev);
         return AVERROR(EINVAL);
     }
-
+    
     memset(values->frame_prev->data[0],16,values->frame_prev->linesize[0]*values->frame_prev->height);
-    memset(values->frame_prev->data[1],128,values->frame_prev->linesize[1]*values->frame_prev->height >> vsub);
-    memset(values->frame_prev->data[2],128,values->frame_prev->linesize[2]*values->frame_prev->height >> vsub);
-
+    memset(values->frame_prev->data[1],128,values->frame_prev->linesize[1]*values->chromah);
+    memset(values->frame_prev->data[2],128,values->frame_prev->linesize[2]*values->chromah);
+    
     av_log(ctx, AV_LOG_DEBUG, "<<< config_props().\n");
-
+    
     return 0;
-
+    
 }
 
 int filter_tout_outlier(uint8_t x, uint8_t y, uint8_t z)
 {
     int dif;
-
-dif =  ((abs(x - y) + abs (z - y) ) / 2) - abs(z-x);
-
+    
+    dif =  ((abs(x - y) + abs (z - y) ) / 2) - abs(z-x);
+    
     //fprintf(stderr,"dif: %d %d\n",dif2,dif1);
-
+    
     // Will make this configurable by command line option.
     return dif>4?1:0;
-
+    
 }
 
 static int filter_tout(AVFrame *p, int x, int y, int w, int h) {
-
+    
     int lw = p->linesize[0];
-
+    
     if ((x-1 < 0) || (x+1 > w) || (y-1 < 0) || (y+1 >= h)) {
         return 0;
     } else {
         int i;
-
+        
         for (i=-1; i<2; i++)
         {
             // detect two pixels above and below (to eliminate interlace artefacts)
@@ -262,14 +262,14 @@ static int filter_tout(AVFrame *p, int x, int y, int w, int h) {
                 if (!filter_tout_outlier(p->data[0][(y-2) * lw + i+x], p->data[0][y * lw + i+x], p->data[0][(y+2) * lw + i+x]))
                     return 0;
             }
-
+            
             if (!filter_tout_outlier(p->data[0][(y-1) * lw + i+x], p->data[0][y * lw + i+x], p->data[0][(y+1) * lw + i+x]))
                 return 0;
-
+            
         }
     }
     return 1;
-
+    
 }
 
 static int filter_vrep_prev;
@@ -278,7 +278,7 @@ static int filter_vrep(AVFrame *p, int x, int y, int w, int h) {
     
     int lw = p->linesize[0];
     int totdiff =0;
-
+    
     if (x != 0 )
         return filter_vrep_prev;
     
@@ -289,10 +289,10 @@ static int filter_vrep(AVFrame *p, int x, int y, int w, int h) {
         
         int y2lw = (y-4) * lw;
         int ylw = y * lw;
-
+        
         
         // do the whole line.
-       for (i=0; i<w; i++)
+        for (i=0; i<w; i++)
         {
             totdiff += abs(p->data[0][y2lw + i] - p->data[0][ylw + i]);
         }
@@ -314,19 +314,19 @@ static int filter_vrep(AVFrame *p, int x, int y, int w, int h) {
 
 static int filter_frame(AVFilterLink *link, AVFrame *in)
 {
-
+    
     AVFilterContext *ctx = link->src;
     valuesContext *values = link->dst->priv;
     AVFilterLink *outlink = link->dst->outputs[0];
     AVFrame *out; // = link->dst->outputs[0]->outpic;
-
+    
     int i,j;
     int cw =0 ,w=0,ow=0,cow=0;
     int yuv;
     int fil;
     char metabuf[128];
     unsigned int histy[DEPTH],histu[DEPTH],histv[DEPTH]; // limited to 8 bit data.
-
+    
     int miny,minu,minv;
     int maxy,maxu,maxv;
     int lowy=-1,lowu=-1,lowv=-1;
@@ -335,11 +335,11 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
     int accy,accu,accv;
     int toty=0,totu=0,totv=0;
     int dify=0,difu=0,difv=0;
-
+    
     int filtot[FILT_NUMB];
-
+    
     av_log(ctx, AV_LOG_DEBUG, ">>> filter_frame().\n");
-
+    
     for (i=0;i<DEPTH;i++)
     {
         histy[i]=0;
@@ -348,68 +348,68 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
     }
     for (i=0; i<FILT_NUMB; i++)
         filtot[i]=0;
-
+    
     out = ff_get_video_buffer(outlink, outlink->w, outlink->h);
     av_frame_copy_props(out, in);
-
+    
     miny = in->data[0][0];
     maxy = in->data[0][0];
-
+    
     minu = in->data[1][0];
     maxu = in->data[1][0];
-
+    
     minv = in->data[2][0];
     maxv = in->data[2][0];
-
+    
     for (j=0; j<link->h; j++) {
         for (i=0;i<link->w;i++) {
-
+            
             yuv = in->data[0][w+i];
-
+            
             if (yuv > maxy) maxy=yuv;
             if (yuv < miny) miny=yuv;
             
             toty += yuv;
-
+            
             histy[yuv]++;
             
             out->data[0][ow+i] = in->data[0][w+i]; // or 16;
-
+            
             dify  += abs(in->data[0][w+i] - values->frame_prev->data[0][w+i]);
-
-        
+            
+            
             if (i<values->chromaw && j<values->chromah) {
                 yuv = in->data[1][cw+i];
                 if (yuv > maxu) maxu=yuv;
                 if (yuv < minu) minu=yuv;
                 totu += yuv;
                 histu[yuv]++;
-
+                
                 
                 yuv = in->data[2][cw+i];
                 if (yuv > maxv) maxv=yuv;
                 if (yuv < minv) minv=yuv;
                 totv += yuv;
                 histv[yuv]++;
-
-
+                
+                
                 difu  += abs(in->data[1][cw+i] - values->frame_prev->data[1][cw+i]);
                 difv  += abs(in->data[2][cw+i] - values->frame_prev->data[2][cw+i]);
-
+                
                 
                 // or 128
                 out->data[1][cow+i] = in->data[1][cow+i];
                 out->data[2][cow+i] = in->data[2][cow+i];
-
+                
             }
-
+            
             // find low / high based on histogram percentile
             
-            lowp = link->h * link->w * 10 / 100;
-            highp = link->h * link->w * 95 / 100;
-            clowp = values->chromah * values->chromaw * 10 / 100;
-            chighp = values->chromah * values->chromaw * 95 / 100;
-
+            lowp = values->fs * 10 / 100;
+            highp = values->fs * 95 / 100;
+            clowp = values->cfs * 10 / 100;
+            chighp = values->cfs * 95 / 100;
+            
             accy = 0; accu=0; accv=0;
             for (i=0; i < DEPTH; i++)
             {
@@ -426,7 +426,7 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
                 
                 if (lowv == -1 && accv >= clowp)
                     lowv = i;
-
+                
                 
                 if (highy == -1 && accy >= highp)
                     highy = i;
@@ -436,13 +436,13 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
                 
                 if (highv == -1 && accv >= chighp)
                     highv = i;
-
+                
                 
             }
             // magic filter
             // options to disable and enable them
             // option to pick one for video out
-
+            
             for (fil = 0; fil < FILT_NUMB; fil ++) {
                 if (values->filter[fil] || values->outfilter == fil)
                 {
@@ -457,75 +457,81 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
                 }
             }
         }
-
+        
         ow += out->linesize[0];
         cow += out->linesize[1];
         
         w += in->linesize[0];
         cw += in->linesize[1];
-        
+        /*
         memcpy(values->frame_prev->data[0] + j * values->frame_prev->linesize[0], in->data[0]+ j * in->linesize[0], link->w);
         if ( j < values->chromah) {
             memcpy(values->frame_prev->data[1] + j * values->frame_prev->linesize[1], in->data[1]+ j * in->linesize[1], values->chromaw);
             memcpy(values->frame_prev->data[2] + j * values->frame_prev->linesize[2], in->data[2]+ j * in->linesize[2], values->chromaw);
         }
+         */
     }
+    
+    av_frame_free(&values->frame_prev);
 
-
+    values->frame_prev->data[0] = in->data[0];
+    values->frame_prev->data[1] = in->data[1];
+    values->frame_prev->data[2] = in->data[2];
+    
     snprintf(metabuf,sizeof(metabuf),"%d",miny);
     av_dict_set(&out->metadata,"lavfi.values.YMIN",metabuf,0);
     
     snprintf(metabuf,sizeof(metabuf),"%d",lowy);
     av_dict_set(&out->metadata,"lavfi.values.YLOW",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%g",1.0 * toty / values->fs);
     av_dict_set(&out->metadata,"lavfi.values.YAVG",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",highy);
     av_dict_set(&out->metadata,"lavfi.values.YHIGH",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",maxy);
     av_dict_set(&out->metadata,"lavfi.values.YMAX",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",minu);
     av_dict_set(&out->metadata,"lavfi.values.UMIN",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",lowu);
     av_dict_set(&out->metadata,"lavfi.values.ULOW",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%g",1.0 * totu / values->cfs);
     av_dict_set(&out->metadata,"lavfi.values.UAVG",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",highu);
     av_dict_set(&out->metadata,"lavfi.values.UHIGH",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",maxu);
     av_dict_set(&out->metadata,"lavfi.values.UMAX",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",minv);
     av_dict_set(&out->metadata,"lavfi.values.VMIN",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",lowv);
     av_dict_set(&out->metadata,"lavfi.values.VLOW",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%g",1.0 * totv / values->cfs);
     av_dict_set(&out->metadata,"lavfi.values.VAVG",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",highv);
     av_dict_set(&out->metadata,"lavfi.values.VHIGH",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%d",maxv);
     av_dict_set(&out->metadata,"lavfi.values.VMAX",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%g",1.0 * difv / values->fs);
     av_dict_set(&out->metadata,"lavfi.values.YDIF",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%g",1.0 * difu / values->cfs);
     av_dict_set(&out->metadata,"lavfi.values.UDIF",metabuf,0);
-
+    
     snprintf(metabuf,sizeof(metabuf),"%g",1.0 * difv / values->cfs);
     av_dict_set(&out->metadata,"lavfi.values.VDIF",metabuf,0);
-
+    
     for (fil = 0; fil < FILT_NUMB; fil ++) {
         if (values->filter[fil]) {
             char metaname[128];
@@ -534,24 +540,24 @@ static int filter_frame(AVFilterLink *link, AVFrame *in)
             av_dict_set(&out->metadata,metaname,metabuf,0);
         }
     }
-
-    if (values->fh != NULL) {
-        fprintf(values->fh,"%d %d %g %d %d %g %d %d %g %d",values->fc,
-                miny,1.0 * toty / values->fs, maxy,
-                minu,1.0 * totu / values->cfs, maxu,
-                minv,1.0 * totv / values->cfs, maxv);
-
-        for (fil = 0; fil < FILT_NUMB; fil ++) {
-            if (values->filter[fil]) {
-                fprintf (values->fh," %g",1.0 * filtot[fil] / values->fs);
-            }
-        }
-
-        fprintf(values->fh,"\n");
-    }
-
+    /*
+     if (values->fh != NULL) {
+     fprintf(values->fh,"%d %d %g %d %d %g %d %d %g %d",values->fc,
+     miny,1.0 * toty / values->fs, maxy,
+     minu,1.0 * totu / values->cfs, maxu,
+     minv,1.0 * totv / values->cfs, maxv);
+     
+     for (fil = 0; fil < FILT_NUMB; fil ++) {
+     if (values->filter[fil]) {
+     fprintf (values->fh," %g",1.0 * filtot[fil] / values->fs);
+     }
+     }
+     
+     fprintf(values->fh,"\n");
+     }
+     */
     values->fc++;
-    av_frame_free(&in);
+//    av_frame_free(&in);
     av_log(ctx, AV_LOG_DEBUG, "<<< filter_frame().\n");
     return ff_filter_frame(outlink, out);
 }
@@ -578,15 +584,15 @@ static const AVFilterPad avfilter_vf_values_outputs[] = {
 AVFilter avfilter_vf_values = {
     .name      = "values",
     .description = ".",
-
+    
     .init = init,
     .uninit    = uninit,
     .query_formats = query_formats,
-
+    
     .priv_size = sizeof(valuesContext),
-
+    
     .inputs    = avfilter_vf_values_inputs,
     .outputs   = avfilter_vf_values_outputs,
     .priv_class = &values_class,
-
+    
 };
